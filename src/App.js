42d166450fde9c3c2321ab12899452084778c98b
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-12-16 22:51:23 +0500

    added new file

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import { Box } from './components/style/App.styled';
+import { Table } from './components/style/App.styled';
 
 
 
 class App extends React.Component {
   render() {
     return (
-      <div style={{ display: 'flex' }}>
-        <Box type='large'>Large</Box>
+      <div>
+        {/* <Box type='large'>Large</Box>
         <Box type='medium'>Medium</Box>
-        <Box type='small'>Small</Box>
+        <Box type='small'>Small</Box> */}
+
+          <Table.Head>
+            <Table.Tr>
+              <Table.Th>Id</Table.Th>
+              <Table.Th>Name</Table.Th>
+              <Table.Th>Status</Table.Th>
+          </Table.Tr>
+          </Table.Head>
+            <Table.Body>
+              <Table.Tr>
+                <Table.Td>1</Table.Td>
+                <Table.Td>Shaxboz</Table.Td>
+                <Table.Td>Student</Table.Td>
+              </Table.Tr>
+              <Table.Tr>
+                <Table.Td>1</Table.Td>
+                <Table.Td>Shaxboz</Table.Td>
+                <Table.Td>Student</Table.Td>
+              </Table.Tr>
+              <Table.Tr>
+                <Table.Td>1</Table.Td>
+                <Table.Td>Shaxboz</Table.Td>
+                <Table.Td>Student</Table.Td>
+              </Table.Tr>
+              <Table.Tr>
+                <Table.Td>1</Table.Td>
+                <Table.Td>Shaxboz</Table.Td>
+                <Table.Td>Student</Table.Td>
+              </Table.Tr>
+            </Table.Body>
       </div>
     );
   }
--- src/components/style/App.styled.jsx
+++ src/components/style/App.styled.jsx
@@ -1,21 +1,51 @@
 import styled, {css} from 'styled-components';
 
-const getSize = ({ type }) => {
-  switch (type) {
-    case 'large':
-      return { size: '200px', color: 'blue' };
-    case 'medium':
-      return { size: '150px', color: 'coral' };
-    case 'small':
-      return { size: '100px', color: 'red' };
-    default:
-      return { size: '150px', color: 'yellow' };
-  }
-};
+// const getSize = ({ type }) => {
+//   switch (type) {
+//     case 'large':
+//       return { size: '200px', color: 'blue' };
+//     case 'medium':
+//       return { size: '150px', color: 'coral' };
+//     case 'small':
+//       return { size: '100px', color: 'red' };
+//     default:
+//       return { size: '150px', color: 'yellow' };
+//   }
+// };
 
-export const Box = styled.div`
-  width: ${(props) => getSize(props).size};
-  height: ${(props) => getSize(props).size};
-  background-color: ${(props) => getSize(props).color};
-  margin: 20px;
+// export const Box = styled.div`
+//   width: ${(props) => getSize(props).size};
+//   height: ${(props) => getSize(props).size};
+//   background-color: ${(props) => getSize(props).color};
+//   margin: 20px;
+// `;
+
+
+export const Table = styled.table`
+  border: 1px solid black;
+  border-collapse: collapse;
+  
+  margin-right: auto;
+  margin-left: auto;
+`;
+
+
+const common = css`
+  border: 1px solid black;
+  width: 40%;
+`;
+
+Table.Body = styled.tbody`
+
+`;
+Table.Head = styled.thead`
+`;
+Table.Tr = styled.tr`
+  ${common}
+`;
+Table.Th = styled.th`
+  ${common}
+`;
+Table.Td = styled.td`
+  ${common}
 `;
