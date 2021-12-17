1ea02223d780f5ce6a6798e10bf1a33ef0ad5b68
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2021-12-17 14:22:56 +0500

    new file

--- src/App.js
+++ src/App.js
@@ -1,49 +1,12 @@
-import React from 'react';
-import { Box } from './components/style/App.styled';
-import { Table } from './components/style/App.styled';
+import React, { Component } from 'react';
+import { Title, Wrapper } from './components/style/App.styled';
 
-
-
-class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
-      <div>
-        {/* <Box type='large'>Large</Box>
-        <Box type='medium'>Medium</Box>
-        <Box type='small'>Small</Box> */}
-
-          <Table.Head>
-            <Table.Tr>
-              <Table.Th>Id</Table.Th>
-              <Table.Th>Name</Table.Th>
-              <Table.Th>Status</Table.Th>
-          </Table.Tr>
-          </Table.Head>
-            <Table.Body>
-              <Table.Tr>
-                <Table.Td>1</Table.Td>
-                <Table.Td>Shaxboz</Table.Td>
-                <Table.Td>Student</Table.Td>
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Td>1</Table.Td>
-                <Table.Td>Shaxboz</Table.Td>
-                <Table.Td>Student</Table.Td>
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Td>1</Table.Td>
-                <Table.Td>Shaxboz</Table.Td>
-                <Table.Td>Student</Table.Td>
-              </Table.Tr>
-              <Table.Tr>
-                <Table.Td>1</Table.Td>
-                <Table.Td>Shaxboz</Table.Td>
-                <Table.Td>Student</Table.Td>
-              </Table.Tr>
-            </Table.Body>
-      </div>
+      <Wrapper>
+        <Title>Hello world!</Title>
+      </Wrapper>
     );
   }
 }
-
-export default App;
--- src/components/style/App.styled.jsx
+++ src/components/style/App.styled.jsx
@@ -21,31 +21,49 @@ import styled, {css} from 'styled-components';
 // `;
 
 
-export const Table = styled.table`
-  border: 1px solid black;
-  border-collapse: collapse;
+// export const Table = styled.table`
+//   border: 1px solid black;
+//   border-collapse: collapse;
   
-  margin-right: auto;
-  margin-left: auto;
-`;
+//   margin-right: auto;
+//   margin-left: auto;
+// `;
 
 
-const common = css`
-  border: 1px solid black;
-  width: 40%;
-`;
+// const common = css`
+//   border: 1px solid black;
+//   width: 40%;
+// `;
 
-Table.Body = styled.tbody`
+// Table.Body = styled.tbody`
 
-`;
-Table.Head = styled.thead`
-`;
-Table.Tr = styled.tr`
-  ${common}
-`;
-Table.Th = styled.th`
-  ${common}
-`;
-Table.Td = styled.td`
-  ${common}
-`;
+// `;
+// Table.Head = styled.thead`
+// `;
+// Table.Tr = styled.tr`
+//   ${common}
+// `;
+// Table.Th = styled.th`
+//   ${common}
+// `;
+// Table.Td = styled.td`
+//   ${common}
+// `;
+
+
+
+
+
+export const Title = styled.h1`
+font-size: 1.5em;
+text-align: center;
+color: coral;
+`
+
+
+export const Wrapper = styled.section`
+padding: 4em;
+background: papayawhip;
+`
+
+
